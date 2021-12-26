29a5bb4a5a2c20f28a80c589d70f58278caf329f
Anastasiia Lobova <anastasiia_lobova@epam.com>
2021-12-26 19:41:04 +0300

    fix spaces

--- src/task2.ts
+++ src/task2.ts
@@ -1,22 +1,23 @@
 import csv from 'csvtojson/v2';
 import CSVError from "csvtojson/v2/CSVError";
-import { WriteStream, createWriteStream } from 'fs';
+import {createWriteStream, WriteStream} from 'fs';
 import path from 'path';
 
 const csvFilePath: string = path.join(__dirname, './csv/nodejs-hw1-ex1.csv');
-const outputFilePath: string =  path.join(__dirname, './csv/nodejs-hw1-ex1.txt');
+const outputFilePath: string = path.join(__dirname, './csv/nodejs-hw1-ex1.txt');
 
 const writeStream: WriteStream = createWriteStream(outputFilePath);
 
 csv()
-    .fromFile(csvFilePath)
-    .subscribe((json) => new Promise((resolve) => {
-        const TRANSFER_SIGN = '\n';
-        writeStream.write(JSON.stringify(json) + TRANSFER_SIGN);
+  .fromFile(csvFilePath)
+  .subscribe((json) => new Promise((resolve) => {
+      const TRANSFER_SIGN: string = '\n';
 
-        resolve();
-    }), (err: CSVError) => {
-        console.error(err);
-    }, () => {
-        console.log('The file was created successfully');
-    });
+      writeStream.write(JSON.stringify(json) + TRANSFER_SIGN);
+
+      resolve();
+  }), (err: CSVError) => {
+      console.error(err);
+  }, () => {
+      console.log('The file was created successfully');
+  });
